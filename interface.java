0be568e98f35e84fe7c46bc5767dae4b74754692
manishasah <manisha.sah@ashoka.edu.in>
2016-04-24 03:28:46 +0530

    Offerer inputs taken and stored to the database.

--- interface.java
+++ interface.java
@@ -6,6 +6,7 @@ CS205
  */
 import java.util.*;
 import java.lang.*;
+import com.mongodb.*;
 
 /** This interface will allow the user to offer a ride and request for offered ride if there are any 
 
@@ -16,25 +17,10 @@ public class Carpool
 	
 	public static void main(String args[])
 	{
-	
-	}
+		User u = new User();
+        	u.user();
+        }
 
-	/* The login method checks whether the user has inputted the correct credentials and 		directs to the user's page where the three options to book, offer or cancel a ride will 		be made available.*/
-	
-	public static void login()
-	{
-		/*User u = new User(); 
-		u.user(id); //calls user method */
-	}
-
-	/*  The signUp method allows the user to create an account, grants access to the 
-		program and on completion redirects to the user’s page.*/
-
-	public static void signUp()
-	{
-		/*User u = new User();
-		u.user(id);  //calls user method */
-	}
 }
 
 /** User class will present the different functions(options) offered by the program and direct to them as per the choices of the user.
@@ -43,9 +29,15 @@ It will take the name of the user from the login or signUp methods to develop th
 
 class User
 {
-	public static void user(String name) 
+	public static void user() 
 	{ 
-		Rider r = new Rider(); // If the users wants to request a ride 
+		System.out.println("Name?");
+		Scanner inp = new Scanner(System.in);
+	        String name = inp.nextLine();
+	
+	        Offer a = new Offer();
+	        a.offer(name);
+		//Rider r = new Rider(); // If the users wants to request a ride 
 	}
 }
 
@@ -55,20 +47,61 @@ ride and confirming it.*/
 
 class Offer
 {
-	String offerer, travelDate, destination, deptTime, numOfSeats, charged;
+	static String offerer;
+	static String travelDate;
+	static String destination;
+	static String deptTime;
+	static String numOfSeats;
+	static String charged;	
 
     /** This method will take inputs from the offerer about the ride. */
 	
 	public static void offer(String name)
-	{
-		
+	{	
+	    System.out.println ("     Hello"+ " "+ name);
+
+            offerer=name;
+
+            System.out.println("Travelling Date (dd/mm/yy)?");
+            Scanner date = new Scanner(System.in);
+            travelDate = date.nextLine();
+
+            System.out.println("Destination?");
+            Scanner dst = new Scanner(System.in);
+            destination = dst.nextLine();
+
+            System.out.println("Departue time (00:00/24 hrs format)?");
+            Scanner time = new Scanner(System.in);
+            deptTime = time.nextLine();
+
+            System.out.println("Maximum no. of Riders?");
+            Scanner seats = new Scanner(System.in);
+            numOfSeats = seats.nextLine();
+
+            System.out.println("Charged (Y/N)?");
+            Scanner chrg= new Scanner(System.in);
+            charged = chrg.nextLine();
+
+            submit();	
+
 	}
 
-		/** The method will store the inputs from offerer and add on the list of offered 			rides.*/ 
+	/** The method will store the inputs from offerer and add on the list of offered rides.*/ 
 		
-	public static void confirm ()
+	public static void submit ()
 	{
-		
+	    MongoClient offer = new MongoClient();
+            DB db = offer.getDB("offer");
+            DBCollection adder = db.getCollection("adder");
+            BasicDBObject info = new BasicDBObject();
+            info.put("Name", offerer);
+            info.put("Travelling Date",travelDate);
+            info.put("Destination", destination);
+            info.put("Departure time", deptTime);
+            info.put("Maximum number of riders", numOfSeats);
+            info.put("Charged", charged);
+
+            adder.insert(info);
 	} 
 }
 
@@ -87,7 +120,7 @@ class Rider
 
 /** The method confirm will accept the select ride and update the list of offered rides.*/
 
-	public static void confirm ()
+	public static void submit ()
 	{
 	
 	}
