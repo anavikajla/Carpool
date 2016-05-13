b3829c356d4a0716e35cb92bb00fb94621f5bcdf
manishasah <manisha.sah@ashoka.edu.in>
2016-05-13 15:55:09 +0530

    Added method to search the rider on offered ride.
    Earlier we associated the method with rider class which we realized was incorrect.

--- Offer.java
+++ Offer.java
@@ -2,7 +2,7 @@ package in.edu.ashoka;
 import com.mongodb.*;
 import java.lang.*;
 
-/** The Offer class allows user to offer a ride by adding the details of the new ride to the list of offered rides.*/
+/** The Offer class allows user to offer a ride by adding the details of the new ride to the list of offered rides and check riders on his/her ride.*/
 public class  Offer{
 
 
@@ -28,6 +28,46 @@ public class  Offer{
         info.put("Description",description);
 
         rides.insert(info);             //inserts all the details in the collection.
+    }
+    
+        /**The method search the ride from the list of confirmed rides based on the details of the offerer
+     and returns the list of his/her confirmed rider*/
+    public static List<ConfirmedRider> searchRiders (String offererName,String offererPhoneNumber)
+    {
+        MongoClient user = new MongoClient();        //connects to the mongodb server.
+        DB db = user.getDB("Carpool");            //creates/uses a "carpool" database to store the values.  
+        DBCollection confirmRides = db.getCollection("ConfirmedRides");          //creates/uses a "rides" collection to store the values.
+
+        BasicDBObject searchQuery = new BasicDBObject();    //ceates a new object to perform search.
+
+
+        //puts the detail of the required ride into the object under specific headings used in the database.
+        searchQuery.put("Offerer Name", offererName.toLowerCase());
+        searchQuery.put("Offerer Phone Number", offererPhoneNumber);
+
+        DBCursor find = confirmRides.find(searchQuery);         //perform search based on the details in the "searchquery" object.
+        DBObject rideInfo;
+
+        List<ConfirmedRider> listOfRiders = new ArrayList<>();      //creates a list of object "RequestRide"
+
+
+        //gets information about the searched ride from the collection one at a time and add the ride to the list.
+        while (find.hasNext())
+        {
+            rideInfo = find.next();
+            String riderName = rideInfo.get("Rider Name").toString();
+            String riderPhoneNumber = rideInfo.get("Rider Phone Number").toString();
+            String numOfSeatsBooked = rideInfo.get("Number of Seats Booked").toString();
+            String deptTime = rideInfo.get("Departure time").toString();
+            String travelDate = rideInfo.get("Travelling Date").toString();
+            String charges = rideInfo.get("Charges").toString();
+            String destination  = rideInfo.get("Destination").toString();
+
+            ConfirmedRider a = new ConfirmedRider(riderName, riderPhoneNumber, numOfSeatsBooked, deptTime,travelDate, charges,destination); ////create a confirmed rider object with given arguments.
+            listOfRiders.add(a);        // //adds "a" ride object to the list of searched rides.
+        }
+        return listOfRiders;
+
     }
 
 }
