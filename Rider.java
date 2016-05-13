f1310b466b0628417f1c144c49c2c6ef5b95282a
anavikajla <anavi1235@gmail.com>
2016-05-13 13:14:09 +0530

    Added methods to search the confirmed rider.
    Earlier it was different class, however since it is associated with rider we included under this class

--- Rider.java
+++ Rider.java
@@ -3,8 +3,8 @@ import com.mongodb.*;
 import java.lang.*;
 import java.util.*;
 
-
-/** The Rider class allows the user to view the offered rides based on their requirements/need.*/
+/** The Rider class allows the user to view the offered rides or the confirmed rides based on their requirements/need
+and returns the list of his/her requested ride or confirmed ride**/
 public class Rider
 {
     
@@ -30,7 +30,8 @@ public class Rider
 
 
         //gets information about the searched ride from the collection one at a time and add the ride to the list.
-        while (find.hasNext()){ 
+        while (find.hasNext())
+        { 
             rideInfo = find.next();
             
             String offererName = rideInfo.get("Name").toString(); 
@@ -45,6 +46,46 @@ public class Rider
             listOfSearchedRides.add(a);  //adds "a" ride object to the list of searched rides.
         }
         return listOfSearchedRides;         
+
+    }
+    
+    /**The method search the ride from the list of confirmed rides based on the details of the offerer
+     and returns the list of his/her confirmed rider*/
+    public static List<ConfirmedRider> searchRiders (String offererName,String offererPhoneNumber)
+    {
+        MongoClient user = new MongoClient();        //connects to the mongodb server.
+        DB db = user.getDB("Carpool");            //creates/uses a "carpool" database to store the values.  
+        DBCollection confirmRides = db.getCollection("ConfirmedRides");          //creates/uses a "rides" collection to store the values.
+
+        BasicDBObject searchQuery = new BasicDBObject();    //ceates a new object to perform search.
+
+
+        //puts the detail of the required ride into the object under specific headings used in the database.
+        searchQuery.put("Offerer Name", offererName.toLowerCase());
+        searchQuery.put("Offerer Phone Number", offererPhoneNumber);
+
+        DBCursor find = confirmRides.find(searchQuery);         //perform search based on the details in the "searchquery" object.
+        DBObject rideInfo;
+
+        List<ConfirmedRider> listOfRiders = new ArrayList<>();      //creates a list of object "RequestRide"
+
+
+        //gets information about the searched ride from the collection one at a time and add the ride to the list.
+        while (find.hasNext())
+        {
+            rideInfo = find.next();
+            String riderName = rideInfo.get("Rider Name").toString();
+            String riderPhoneNumber = rideInfo.get("Rider Phone Number").toString();
+            String numOfSeatsBooked = rideInfo.get("Number of Seats Booked").toString();
+            String deptTime = rideInfo.get("Departure time").toString();
+            String travelDate = rideInfo.get("Travelling Date").toString();
+            String charges = rideInfo.get("Charges").toString();
+            String destination  = rideInfo.get("Destination").toString();
+
+            ConfirmedRider a = new ConfirmedRider(riderName, riderPhoneNumber, numOfSeatsBooked, deptTime,travelDate, charges,destination); ////create a confirmed rider object with given arguments.
+            listOfRiders.add(a);        // //adds "a" ride object to the list of searched rides.
+        }
+        return listOfRiders;
 
     }
 }
